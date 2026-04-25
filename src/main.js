9af57ef29d11bfb0708c5d70b98fcb2d42aacb2b
João Victor <pessoal.jv.nascimento@gmail.com>
2026-04-25 16:51:44 -0300

    adiciona media query tablet-768px e botão reset-button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './style.css'
+import './tablet-768px.css'
+
+// importa imagem do background
+import backgroundImage from './assets/background.jpeg'
+// adiciona a imagem no estilo
+document.getElementsByTagName('main')[0].style.backgroundImage = `url(${backgroundImage})`
